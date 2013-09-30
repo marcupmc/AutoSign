22f80e5a77e826770592576af20a6ded02923f0a
marcupmc <marc.gregoire1989@gmail.com>
2013-09-30 10:54:00 +0200

    wihtoyut ip

--- www/js/index.js
+++ www/js/index.js
@@ -74,20 +74,20 @@ $(document).ready( function () {
 
 
 function myIP() {
-	if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-	else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//	if (window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
+//	else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+//
+//	xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
+//	xmlhttp.send();
+//
+//	hostipInfo = xmlhttp.responseText.split("\n");
+//
+//	for (i=0; hostipInfo.length >= i; i++) {
+//		ipAddress = hostipInfo[i].split(":");
+//		if ( ipAddress[0] == "IP" ) return ipAddress[1];
+//	}
 
-	xmlhttp.open("GET","http://api.hostip.info/get_html.php",false);
-	xmlhttp.send();
-
-	hostipInfo = xmlhttp.responseText.split("\n");
-
-	for (i=0; hostipInfo.length >= i; i++) {
-		ipAddress = hostipInfo[i].split(":");
-		if ( ipAddress[0] == "IP" ) return ipAddress[1];
-	}
-
-	return false;
+	return "127.0.0.1";
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------
